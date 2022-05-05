58fd3523aedfaba4ea0e26227f962a508a6f203d
zeroTwo <88587913+ZeroTwo-Dog@users.noreply.github.com>
2022-05-05 12:12:36 +0900

    feat: 스프링에서 기본으로 적용해주는 빈 후처리기 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -9,15 +9,20 @@ import hello.proxy.config.v2_proxy.interface_proxy.v1_proxy.InterfaceProxyConfig
 import hello.proxy.config.v3_proxyfactroy.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactroy.ProxyFactoryConfigV2;
 import hello.proxy.config.v4_postProcessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 //@Import({InterfaceProxyConfig.class, InterfaceProxyConfig2.class})
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
+//@Import(BeanPostProcessorConfig.class)
 //@Import(ProxyFactoryConfigV2.class)
 //@Import(ProxyFactoryConfigV1.class)
 //@Import({AppV2Config.class ,AppV1Config.class})
@@ -26,6 +31,11 @@ public class ProxyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 
+	}
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
 	}
 
 }
---
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,34 @@
+package hello.proxy.config.v5_autoproxy;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactroy.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Created by park on 2022/05/05.
+ */
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class AutoProxyConfig {
+
+
+  @Bean
+  public Advisor advisor1 (LogTrace logTrace) {
+    //pointcut
+    NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+    pointcut.setMappedNames("request*", "order*", "save*");
+    //advice
+    LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+    //advisor = pointcut + advice
+    return new DefaultPointcutAdvisor(pointcut, advice);
+
+  }
+
+}
